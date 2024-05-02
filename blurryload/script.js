e3c8b8d65a67495d6c84c7a6577c0bccdb286bf3
jerwin-yottron-learning <jerwinyottron@gmail.com>
2024-05-02 23:30:27 +0530

    #blur

--- blurryload/script.js
+++ blurryload/script.js
@@ -1,1 +1,19 @@
 console.log('hi');
+
+const loadText =document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+let load =0;
+
+let int =setInterval(blur,30);
+
+function blur(){
+    load++
+    if(load>99){
+        clearInterval(int)
+    }
+    console.log(load);
+    loadText.innerText =`${load}%`;
+    loadText.style.opacity = (100 - load) / 100;
+    const blurAmount = 10 * ((100 - load) / 100); 
+    bg.style.filter = `blur(${blurAmount}px)`;
+}
