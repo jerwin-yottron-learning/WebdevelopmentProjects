26e528d65e2d7266a7672b394acf8a165c04583e
jerwin-yottron-learning <jerwinyottron@gmail.com>
2024-05-04 01:09:50 +0530

    jokeGenerator

---
+++ Joke/script.js
@@ -0,0 +1,17 @@
+const jokeEle = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokebtn');
+jokeBtn.addEventListener('click',generateJoke)
+generateJoke();
+
+function generateJoke(){
+    const config={
+        headers:{
+            'Accept':'application/json'
+        }
+    }
+
+    fetch('https://icanhazdadjoke.com',config)
+    .then((res)=>res.json()).then((data)=>{
+        jokeEle.innerHTML = data.joke
+    })
+}
